8fb6c3004b844603f8920bf7f1e48219d55210f6
Erick140297 <erickmonterrubio67@outlook.com>
2023-01-20 09:26:59 -0600

    search

--- src/routes/routes_product/get_products.js
+++ src/routes/routes_product/get_products.js
@@ -4,11 +4,25 @@ const router = Router();
 
 // Para esta ruta no es necesario añadir algún valor extra, ya que esta ruta trae todas las ordenes hechas.
 
-
 router.get("/products", async (req, res) => {
   try {
-    const products = await Product.find()
-    res.status(200).json(products);
+    const { name, subCategory } = req.query;
+    if (name !== undefined && subCategory === undefined) {
+      const products = await Product.find({
+        name: { $regex: name, $options: "i" },
+      });
+      res.status(200).json(products);
+      return;
+    } else if (name === undefined && subCategory !== undefined) {
+      const products = await Product.find({
+        name: { $regex: subCategory, $options: "i" },
+      });
+      res.status(200).json(products);
+      return;
+    } else {
+      const products = await Product.find();
+      res.status(200).json(products);
+    }
   } catch (error) {
     res.status(400).json(error);
   }
