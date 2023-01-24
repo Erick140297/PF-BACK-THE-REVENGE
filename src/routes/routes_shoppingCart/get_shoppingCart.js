836929b4f490ec6f6020f6b3d84a2e4dd553af64
Erick140297 <erickmonterrubio67@outlook.com>
2023-01-24 00:09:29 -0600

    Routes shoppingCart

--- src/routes/routes_shoppingCart/get_shoppingCart.js
+++ src/routes/routes_shoppingCart/get_shoppingCart.js
@@ -1,19 +1,31 @@
-const express = require('express');
+const express = require("express");
 const ShoppingCart = require("../../models/shoppingCart");
+const User = require("../../models/user");
 
 const router = express.Router();
 
 // Para esta ruta es necesario poner el id del carrito exacto que queremos ver. "id"
 // Verificar siempre la ruta a la que nos dirigimos y respetar la barrita --> "/" después de un "/shoppingCart" para que logre funcionar correctamente.
 
-router.get('/shoppingCart/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const cart = await ShoppingCart.findById(id).populate("items.product");
-        res.status(200).json({ cart });  
-    } catch (error) {
-        res.status(404).json(error);
+router.get("/shoppingCart/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (user.cart.length === 0) {
+      res.status(200).json({ message: "Tu carrito esta vácio" });
+    } else {
+      const cart = await ShoppingCart.findById(user.cart).populate(
+        "items.product"
+      );
+      if (cart.items.length === 0) {
+        res.status(200).json({ message: "Tu carrito esta vácio" });
+      } else {
+        res.status(200).json(cart);
+      }
     }
+  } catch (error) {
+    res.status(404).json(error);
+  }
 });
 
 module.exports = router;
--- src/routes/routes_shoppingCart/post_shoppingCart.js
+++ src/routes/routes_shoppingCart/post_shoppingCart.js
@@ -8,30 +8,41 @@ const ShoppingCart = require("../../models/shoppingCart");
 // nuevos productos si es el caso. :)
 
 router.post("/shoppingCart", async (req, res) => {
-    try {
-        const {productId, userId} = req.body
-        let user = await User.findById(userId);
-        console.log(productId);
-        console.log(userId);
-        if(user.cart.length === 0) {
-            const newCart = new ShoppingCart({
-                items: [{product: productId}],
-                user: user._id
-            });
-
-            const saveCart = await newCart.save();
-            user.cart = user.cart.concat(saveCart._id);
-            user.save();
-            res.status(200).json(saveCart);
-        } else {
-            let cart = await ShoppingCart.findById(user.cart[0]);
-            cart.items = cart.items.concat([{product: productId}]);
-            const saveCart = await cart.save();
-            res.status(200).json(saveCart);
-        }
-    } catch (error) {
-        res.status(404).json(error);
+  try {
+    const { productId, userId } = req.body;
+    let user = await User.findById(userId);
+    if (user.cart.length === 0) {
+      // Crear un nuevo carrito si el usuario no tiene uno
+      const newCart = new ShoppingCart({
+        items: [{ product: productId }],
+        user: user._id,
+      });
+      const saveCart = await newCart.save();
+      user.cart = user.cart.concat(saveCart._id);
+      user.save();
+      res.status(200).json(saveCart);
+    } else {
+      // Buscar el carrito del usuario
+      let cart = await ShoppingCart.findById(user.cart[0]);
+      // Buscar el índice del producto en el arreglo de items
+      const index = cart.items.findIndex(
+        (item) => item.product.toString() === productId
+      );
+      // Verificar si el producto ya está en el arreglo de items
+      if (index !== -1) {
+        // Si el producto ya está en el arreglo, solo modificar la cantidad
+        cart.items[index].quantity += 1;
+      } else {
+        // Si el producto no está en el arreglo, agregar un nuevo item
+        cart.items = cart.items.concat([{ product: productId }]);
+      }
+      // Guardar el carrito actualizado en la base de datos
+      const saveCart = await cart.save();
+      res.status(200).json(saveCart);
     }
-})
+  } catch (error) {
+    res.status(404).json(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
