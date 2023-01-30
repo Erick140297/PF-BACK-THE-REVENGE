2aba5472f0d29b885021cf19293ea0f6d54a1b40
Erick140297 <erickmonterrubio67@outlook.com>
2023-01-30 13:44:21 -0600

    userEnable

--- src/models/user.js
+++ src/models/user.js
@@ -1,44 +1,48 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-        },
-        city: String,
-        address: String,
-        phone: String,
-        admin: {
-            type: Boolean,
-            default: false,
-        },
-        payMethod: Object,
-        deleteLogic: {
-            type: Boolean,
-            default: false,
-        },
-        cart:[
-            {
-                type: Schema.Types.ObjectId,
-                ref: "ShoppingCart",
-            }
-        ],
-        orders: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: "PurchaseOrder"
-            }
-        ]
+  {
+    name: {
+      type: String,
+      required: true,
     },
-{
+    email: {
+      type: String,
+      required: true,
+    },
+    city: String,
+    address: String,
+    phone: String,
+    admin: {
+      type: Boolean,
+      default: false,
+    },
+    payMethod: Object,
+    deleteLogic: {
+      type: Boolean,
+      default: false,
+    },
+    cart: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "ShoppingCart",
+      },
+    ],
+    orders: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "PurchaseOrder",
+      },
+    ],
+    enabled: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
     timestamps: false,
     versionKey: false,
-}
+  }
 );
 
 const User = model("User", userSchema);
