e23bf9db5aedcdd4146f72afa17a0d38265e8feb
Erick140297 <erickmonterrubio67@outlook.com>
2023-02-01 16:41:07 -0600

    routesOrder

--- src/routes/routes_purchaseOrder/get_purchaseOrders.js
+++ src/routes/routes_purchaseOrder/get_purchaseOrders.js
@@ -1,16 +1,16 @@
 const { Router } = require("express");
 const router = Router();
 const PurchaseOrder = require("../../models/purchaseOrder");
-
+const User = require("../../models/user"); 
 // Para esta ruta no es necesario añadir algún valor extra, ya que esta ruta trae todas las ordenes hechas.
 
 router.get("/order", async (req, res) => {
   try {
     const { userId } = req.query;
-
     if (userId) {
+      const user = await User.findById(userId)
       const orders = await PurchaseOrder.find({ user: userId });
-      res.status(200).json(orders);
+      res.status(200).json({orders, email: user.email, name: user.name});
     } else {
       const orders = await PurchaseOrder.find();
       res.status(200).json(orders);
