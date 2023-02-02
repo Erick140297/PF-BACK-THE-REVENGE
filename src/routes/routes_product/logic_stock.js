9045f6f4f85c876ecbd1bd5138b07fd267cee62c
Erick140297 <erickmonterrubio67@outlook.com>
2023-02-02 14:14:20 -0600

    stock deploy

--- src/routes/routes_product/logic_stock.js
+++ src/routes/routes_product/logic_stock.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const axios = require("axios");
-const Product = require("../../models/product");
 const PurchaseOrder = require("../../models/purchaseOrder");
 const router = Router();
 
@@ -9,14 +8,15 @@ router.put("/logic/stock/:orderId", async (req, res) => {
     const { orderId } = req.params;
     const order = await PurchaseOrder.findById(orderId);
 
-    const promises = await order.cart.items.map((e) => {
-      return axios.put(
-        `http://localhost:3001/stock/${e.product._id}/${e.quantity}`
+    const promises = await order.cart.items.map(async (e) => {
+      return await axios.put(
+        `https://pf-back-the-revenge-production.up.railway.app/stock/${e.product._id}/${e.quantity}`
       );
     });
-    const result = await Promise.all(promises);
+   
+    await Promise.all(promises);
 
-    res.status(200).json(result);
+    res.status(200).json("ok");
   } catch (error) {
     res.status(400).json(error);
   }
